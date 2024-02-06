820bf8edd0084f7c3cb7a1414b3936135056ac7c
Amogh Hegde <amoghhegde3@gmail.com>
2024-02-06 11:55:14 +0530

    solved

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,15 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let sum =0 ;
+    let before = new Date().getTime();
+    for(let i =0 ; i<n ; i++){
+        sum += i;
+    }
+    let after = new Date().getTime();
+    console.log("the sum is ", sum);
+    let tot = (after - before)/1000;
+    return tot;
 }
+
+console.log(calculateTime(1000000000));
